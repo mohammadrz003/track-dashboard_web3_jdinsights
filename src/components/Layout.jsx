e5aca10280402004cb08a61c1f64720f3475c625
Mohammad Rezaii <rezaiimohammad003@gmail.com>
2023-08-04 18:51:02 +0330

    init commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+const Layout = () => {
+  return (
+    <div className="container mx-auto max-w-screen-xl">
+      <Header />
+    </div>
+  );
+};
+
+export default Layout;
